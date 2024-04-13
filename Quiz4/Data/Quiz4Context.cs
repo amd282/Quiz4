65b80490bb8f41aaaa35a9aba8f842d9a443440f
amd282 <amd282@yahoo.com>
2024-04-13 13:30:15 -0400

    Initial commit

---
+++ Quiz4/Data/Quiz4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace Quiz4.Data
+{
+    public class Quiz4Context : DbContext
+    {
+        public Quiz4Context (DbContextOptions<Quiz4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
